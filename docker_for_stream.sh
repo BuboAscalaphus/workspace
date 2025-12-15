35c347d27b76fb7b69d4c22b745a11f5d7ad2975
BuboAscalaphus <davide.botturi@prospecto.cloud>
2025-12-15 11:24:42 +0100

    added host network to ethernet docker

--- docker_for_stream.sh
+++ docker_for_stream.sh
@@ -1,1 +1,6 @@
-docker run --rm --name mediamtx   -p 8554:8554   -e MTX_READTIMEOUT=1000s   -e MTX_WRITETIMEOUT=100s   bluenviron/mediamtx:latest
+docker run --rm --name mediamtx \
+  --network host \
+  -e MTX_READTIMEOUT=1000s \
+  -e MTX_WRITETIMEOUT=100s \
+  bluenviron/mediamtx:latest
+
